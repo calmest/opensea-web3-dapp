a1d69a1f06d78bdf6f148f3e9523174a525ca493
Chukwuebuka Okafor <okaforgreat@gmail.com>
2022-06-10 22:54:49 +0100

    First Commit

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import { useWeb3 } from '@3rdweb/hooks'
+import Head from 'next/head'
+import Header from '../components/Header'
+import Hero from '../components/Hero'
+
+const style = {
+  wrapper: ``,
+  walletConnectWrapper: `flex flex-col justify-center items-center h-screen w-screen bg-[#3b3d42] `,
+  button: `border border-[#282b2f] bg-[#2081e2] p-[0.8rem] text-xl font-semibold rounded-lg cursor-pointer text-black`,
+  details: `text-lg text-center text=[#282b2f] font-semibold mt-4`,
+}
+
+export default function Home() {
+  const { address, connectWallet } = useWeb3()
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  )
+}
